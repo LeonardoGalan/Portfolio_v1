719a125ca27ca6ba7170fd1abd10cb95ddab0282
leonardogalan <leonardogalan2@gmail.com>
2024-08-15 20:29:27 -0400

    slightly increased font size for jobs on tab list, and removed sidebar from appearing in small screens

--- src/Components/ExpTabs.jsx
+++ src/Components/ExpTabs.jsx
@@ -5,39 +5,39 @@ import "../styles/ExpTabs.css"
 export default function ExpTab() {
 
         const experienceItems = {
-            HealthFirst: {
-                jobTitle: "Software Engineer Intern @ ",
-                duration: "JUN 2023 - AUG 2023",
-                desc: [
-                    "Engineered a responsive UI/UX for HealthFirst’s Patient 360 web application, leveraging React and TypeScript. This project for HealthFirst - a leading health insurance provider, serving over 1.8 million members in New York, significantly improved the accessibility of patient data and increased efficiency for healthcare professionals.",
-                    "Collaborated with the frontend and backend team on UI design and information architecture, introducing user-centric features (e.g., drag-and-drop functionality for moving patient data tables around, hide/show certain data information such as patient vitals, medications, care team, etc), which enhanced overall application usability"
-                ]
-            },
-            "HeadStarter AI": {
-                jobTitle: "Software Engineer Fellow @ ",
-                duration: "JUL 2024 - PRESENT",
-                desc: [
-                    "Building 5 AI driven applications utilizing React, JavaScript, NextJS and AWS technologies",
-                    "Participated in weekly hackathons, collaborated on 3 projects on 4 person teams, and contributed to the code base of a startup company."
-                ]
-            },
-            "Scarce City": {
-                jobTitle: "Software Engineer Intern @ ",
-                duration: "JUN 2022 - AUG 2022",
-                desc: [
-                    "Improved user experience of Scarce City - a Bitcoin goods marketplace by developing, structuring, and testing new features using TypeScript, NextJS, and Docker with a PostgreSQL database.",
-                    "Rendered creator images from the backend into the frontend to display an avatar logo for each associated digital/physical item being sold."
-                ]
-            },
-            "CUNY Tech Talent Pipeline Bootcamp": {
-                jobTitle: "Web Development Fellow @ ",
-                duration: "DEC 2021 - AUG 2022",
-                desc: [
-                    "One of 50 selected to participate in the intensive NYC Tech Talent Pipeline Web Development Residency program which provides hands-on training in full-stack web development.",
-                    "Worked on a total of 10 projects as part of 4 different teams, utilizing JavaScript, ReactJS, HTML, CSS, RESTful APIs, Heroku, database management systems, and functional programming."
-                ]
-            },
-        }
+          HealthFirst: {
+            jobTitle: "Software Engineer Intern @ ",
+            duration: "JUN 2023 - AUG 2023",
+            desc: [
+              "Engineered a responsive UI/UX for HealthFirst’s Patient 360 web application, leveraging React and TypeScript. This app for HealthFirst - a leading health insurance provider, serving over 1.8 million members in New York, hopes to improve accessibility of patient data and increased efficiency for healthcare professionals.",
+              "Designed user-centric features such as drag-and-drop functionality for moving patient data tables, hide/show certain data information such as patient vitals, medications, which enhanced overall application usability",
+            ],
+          },
+          "HeadStarter AI": {
+            jobTitle: "Software Engineer Fellow @ ",
+            duration: "JUL 2024 - PRESENT",
+            desc: [
+              "Building 5 AI driven applications utilizing React, JavaScript, NextJS and AWS technologies",
+              "Participated in weekly hackathons, collaborated on 3 projects on 4 person teams, and contributed to the code base of a startup company.",
+            ],
+          },
+          "Scarce City": {
+            jobTitle: "Software Engineer Intern @ ",
+            duration: "JUN 2022 - AUG 2022",
+            desc: [
+              "Improved user experience of Scarce City - a Bitcoin goods marketplace by developing, structuring, and testing new features using TypeScript, NextJS, and Docker with a PostgreSQL database.",
+              "Rendered creator images from the backend into the frontend to display an avatar logo for each associated digital/physical item being sold.",
+            ],
+          },
+          "CUNY Tech Talent Pipeline Bootcamp": {
+            jobTitle: "Web Development Fellow @ ",
+            duration: "DEC 2021 - AUG 2022",
+            desc: [
+              "One of 50 selected to participate in the intensive NYC Tech Talent Pipeline Web Development Residency program which provides hands-on training in full-stack web development.",
+              "Worked on a total of 10 projects as part of 4 different teams, utilizing JavaScript, ReactJS, HTML, CSS, RESTful APIs, Heroku, database management systems, and functional programming.",
+            ],
+          },
+        };
     
         const [value, setValue] = React.useState(Object.keys(experienceItems)[0]);
 
